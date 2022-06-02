6366dc7a125e6d828d5f9905b3b08a0ef29000e3
matiasfeliu92 <matumazparrote@gmail.com>
2022-06-01 21:02:53 -0300

    agrego middlewares a app.ts

--- server/app.ts
+++ server/app.ts
@@ -6,8 +6,4 @@ app.use(morgan('dev'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server on port ", process.env.PORT || 3000)
-})
-
 export default app
