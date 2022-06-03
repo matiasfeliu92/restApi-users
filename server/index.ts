ed08341a437f78dc2ab90904ae21950dadd2b502
matiasfeliu92 <matumazparrote@gmail.com>
2022-06-03 18:34:37 -0300

    funcion main para conectar con mongodb e iniciar app

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,17 @@
 import app from './app'
+import { connect } from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-    console.log("http://localhost:"+port)
-})
+async function main() {
+    const db = await connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.lz54k.mongodb.net/${process.env.DBNAME}retryWrites=true&w=majority`, ()=>{
+        console.log('Base de datos conectada')
+        app.listen(port, () => {
+            console.log("http://localhost:"+port)
+        })
+    })
+}
+
+main()
