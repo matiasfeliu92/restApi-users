00348d9df28fc0790812ccac047cdc258b5f362b
matiasfeliu92 <matumazparrote@gmail.com>
2022-06-03 09:53:33 -0300

    ajustes en el puerto de la app

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,7 @@
 import app from './app'
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server on port", process.env.PORT || 3000)
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log("http://localhost:"+port)
 })
