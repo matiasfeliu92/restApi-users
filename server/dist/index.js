b5ad0546e8c38b16ae09ff9cd46a360f953cccf6
matiasfeliu92 <matumazparrote@gmail.com>
2022-06-03 10:05:59 -0300

    cambios en archivos js compilados

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-app_1.default.listen(process.env.PORT || 3000, () => {
-    console.log("Server on port", process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
+app_1.default.listen(port, () => {
+    console.log("http://localhost:" + port);
 });
