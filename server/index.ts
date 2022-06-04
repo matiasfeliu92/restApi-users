2e00a6f61c6353eb96291746272e43ba49a1a6e0
matiasfeliu92 <matumazparrote@gmail.com>
2022-06-03 21:27:19 -0300

    ajustes en mongoDb URI

--- server/index.ts
+++ server/index.ts
@@ -4,9 +4,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const port = process.env.PORT || 3000
-
+//`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.lz54k.mongodb.net/${process.env.DBNAME}retryWrites=true&w=majority`
 async function main() {
-    const db = await connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.lz54k.mongodb.net/${process.env.DBNAME}retryWrites=true&w=majority`, ()=>{
+    const db = await connect("mongodb+srv://youtube_vet:VJq6w8eZ6nJFu0aQ@cluster0.fvcxm.mongodb.net/typescript-mongodb-restapiretryWrites=true&w=majority", ()=>{
         console.log('Base de datos conectada')
         app.listen(port, () => {
             console.log("http://localhost:"+port)
