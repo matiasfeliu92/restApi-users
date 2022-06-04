1551e389e16f0cf0413bdb74413932f36cab3cdf
matiasfeliu92 <matumazparrote@gmail.com>
2022-06-03 21:26:33 -0300

    creacion de modelo de user para mongodb

---
+++ server/models/user.ts
@@ -0,0 +1,23 @@
+import {model, Schema, Document} from 'mongoose'
+import bcrypt from 'bcrypt'
+
+export interface IUser extends Document {
+    email: String,
+    password: String
+}
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+export default model<IUser>('user', userSchema)
