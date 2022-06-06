3dedddd71c1c378520e91746d846370ac0bb343c
matiasfeliu92 <matumazparrote@gmail.com>
2022-06-06 11:17:11 -0300

    endpoint de prueba

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,15 @@
 import express from 'express'
+import {Request, Response,} from 'express'
 import morgan from 'morgan'
 const app = express()
 
 app.use(morgan('dev'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+
+//Endpoint de prueba
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hellooo Worlddd')
+})
 
 export default app
