dc9b599fcd78b932a7a1bc8eb11431fb6173c6d5
matiasfeliu92 <matumazparrote@gmail.com>
2022-06-07 18:16:33 -0300

    cambios en index ts para no usar mongo db

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,20 @@
 import app from './app'
-import { connect } from 'mongoose'
+//import { connect } from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const port = process.env.PORT || 3000
 //`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.lz54k.mongodb.net/${process.env.DBNAME}retryWrites=true&w=majority`
 async function main() {
-    const db = await connect("mongodb+srv://youtube_vet:VJq6w8eZ6nJFu0aQ@cluster0.fvcxm.mongodb.net/typescript-mongodb-restapiretryWrites=true&w=majority", ()=>{
-        console.log('Base de datos conectada')
+    // const db = await connect("mongodb+srv://youtube_vet:VJq6w8eZ6nJFu0aQ@cluster0.fvcxm.mongodb.net/typescript-mongodb-restapiretryWrites=true&w=majority", ()=>{
+    //     console.log('Base de datos conectada')
+    //     app.listen(port, () => {
+    //         console.log("http://localhost:"+port)
+    //     })
+    // })
         app.listen(port, () => {
             console.log("http://localhost:"+port)
         })
-    })
 }
 
 main()
