8e88e363e74a54539f1171d16a4187dbf3de561a
matiasfeliu92 <matumazparrote@gmail.com>
2022-06-07 18:42:15 -0300

    archivo db.js compilado yactualizado

--- server/dist/database/db.js
+++ server/dist/database/db.js
@@ -1,11 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.pool = void 0;
 const pg_1 = require("pg");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 exports.pool = new pg_1.Pool({
-    user: 'matias92',
-    host: 'postgresql-matias92.alwaysdata.net',
-    password: 'matute1592',
-    database: 'matias92_restapitypescriptpostgre1',
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     port: 5432,
 });
