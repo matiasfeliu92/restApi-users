359441283d635fc5c232f4cc64c9f47450293c92
matiasfeliu92 <matumazparrote@gmail.com>
2022-06-08 10:38:20 -0300

    creacion del controlador que obtiene todos los users de la DB

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,3 +1,12 @@
 import {Request, Response} from 'express'
 import {pool} from '../database/db'
 import { QueryResult } from 'pg'
+
+export const getAllUsers = async (req: Request, res: Response) => {
+    const users: QueryResult = await pool.query('SELECT * FROM users')
+    if(users){
+        return res.status(200).json(users.rows)
+    }else{
+        return res.status(403).json({message: 'no existen usuarios'})
+    }
+}
