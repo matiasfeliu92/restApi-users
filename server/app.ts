dccf4a3c645b13beb5cd8c6674e4022944998019
matiasfeliu92 <matumazparrote@gmail.com>
2022-06-21 18:10:25 -0300

    agregue las rutas de auth en app

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import express, {Application} from 'express'
 import {Request, Response,} from 'express'
 import morgan from 'morgan'
 import userRoutes from './routes/usersRouter'
+import authRoutes from './routes/authRouter'
 const app = express()
 
 app.use(morgan('dev'))
@@ -14,5 +15,6 @@ app.get('/', (req: Request, res: Response) => {
 })
 
 app.use('/users', userRoutes)
+app.use('/auth', authRoutes)
 
 export default app
