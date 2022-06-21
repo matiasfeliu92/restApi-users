a92480283abe40ecf564e51634edff76a0792ef4
matiasfeliu92 <matumazparrote@gmail.com>
2022-06-21 18:09:43 -0300

    ajustes en userRouter

--- server/routes/usersRouter.ts
+++ server/routes/usersRouter.ts
@@ -1,10 +1,9 @@
 import { Router } from "express"
-import { createUser, getAllUsers, getUsersById, updateUser } from "../controllers/userController"
+import { getAllUsers, getUsersById, updateUser } from "../controllers/userController"
 const router = Router()
 
 router.get('/', getAllUsers)
 router.get('/:id', getUsersById)
-router.post('/create', createUser)
 router.put('/update/:id', updateUser)
 
 export default router
