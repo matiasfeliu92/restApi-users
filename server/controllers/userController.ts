9ab4da99c4c0d4759421ad50064852e284165b5f
matiasfeliu92 <matumazparrote@gmail.com>
2022-06-21 10:46:25 -0300

    creacion de los controladors para crear un usuario y actualizar

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,22 +1,55 @@
 import {Request, Response} from 'express'
 import {pool} from '../database/db'
 import { QueryResult } from 'pg'
+import {User} from '../interface/usersInterface'
+import bcrypt from 'bcrypt'
 
 export const getAllUsers = async (req: Request, res: Response) => {
-    const users: QueryResult = await pool.query('SELECT * FROM users')
-    if(users){
-        return res.status(200).json(users.rows)
-    }else{
-        return res.status(403).json({message: 'no existen usuarios'})
+    try {
+        const users: QueryResult = await pool.query('SELECT * FROM users ORDER BY id ASC')
+        res.json(users.rows)
+    } catch (error) {
+        if(error instanceof Error){
+            res.status(403).json({message: error.message})
+        }
     }
 }
 
 export const getUsersById = async (req: Request, res: Response) => {
-    const id = parseInt(req.params.id)
-    const user: QueryResult = await pool.query('SELECT * FROM users where id = $1', [id])
-    if(user){
-        return res.status(200).json(user.rows)
-    }else{
-        res.status(403).json({message: 'no existe el usuario buscado'})
+    try {
+        const {id} = req.params
+        const user: QueryResult = await pool.query('SELECT * FROM users where id = $1', [id])
+        res.json(user.rows)
+    } catch (error) {
+        if(error instanceof Error){
+            res.status(403).json({message: error.message})
+        }
+    }
+}
+
+export const createUser = async (req: Request, res: Response) => {
+    try {
+        const {name, age, country, email, password}: User = req.body
+        const hashPass = bcrypt.hashSync(password, 5)
+        await pool.query('INSERT INTO users (name, age, country, email, password) VALUES ($1, $2, $3, $4, $5)', [name, age, country, email, hashPass])
+        res.json('new user registered')
+    } catch (error) {
+        if(error instanceof Error){
+            res.status(403).json({message: error.message})
+        }
+    }
+}
+
+export const updateUser = async (req: Request, res: Response) => {
+    try {
+        const {name, age, country, email, password}: User = req.body
+        const {id} = req.params
+        const hashPass = bcrypt.hashSync(password, 5)
+        await pool.query('UPDATE users  set name = $1, age = $2, country = $3, email = $4, password = $5 WHERE id = $6', [name, age, country, email, hashPass, id])
+        res.json('new user updated')
+    } catch (error) {
+        if(error instanceof Error){
+            res.status(403).json({message: error.message})
+        }
     }
 }
--- server/dist/controllers/userController.js
+++ server/dist/controllers/userController.js
@@ -8,27 +8,64 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUsersById = exports.getAllUsers = void 0;
+exports.updateUser = exports.createUser = exports.getUsersById = exports.getAllUsers = void 0;
 const db_1 = require("../database/db");
+const bcrypt_1 = __importDefault(require("bcrypt"));
 const getAllUsers = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const users = yield db_1.pool.query('SELECT * FROM users');
-    if (users) {
-        return res.status(200).json(users.rows);
+    try {
+        const users = yield db_1.pool.query('SELECT * FROM users ORDER BY id ASC');
+        res.json(users.rows);
     }
-    else {
-        return res.status(403).json({ message: 'no existen usuarios' });
+    catch (error) {
+        if (error instanceof Error) {
+            res.status(403).json({ message: error.message });
+        }
     }
 });
 exports.getAllUsers = getAllUsers;
 const getUsersById = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const id = parseInt(req.params.id);
-    const user = yield db_1.pool.query('SELECT * FROM users where id = $1', [id]);
-    if (user) {
-        return res.status(200).json(user.rows);
+    try {
+        const { id } = req.params;
+        const user = yield db_1.pool.query('SELECT * FROM users where id = $1', [id]);
+        res.json(user.rows);
     }
-    else {
-        res.status(403).json({ message: 'no existe el usuario buscado' });
+    catch (error) {
+        if (error instanceof Error) {
+            res.status(403).json({ message: error.message });
+        }
     }
 });
 exports.getUsersById = getUsersById;
+const createUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { name, age, country, email, password } = req.body;
+        const hashPass = bcrypt_1.default.hashSync(password, 5);
+        yield db_1.pool.query('INSERT INTO users (name, age, country, email, password) VALUES ($1, $2, $3, $4, $5)', [name, age, country, email, hashPass]);
+        res.json('new user registered');
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            res.status(403).json({ message: error.message });
+        }
+    }
+});
+exports.createUser = createUser;
+const updateUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { name, age, country, email, password } = req.body;
+        const { id } = req.params;
+        const hashPass = bcrypt_1.default.hashSync(password, 5);
+        yield db_1.pool.query('UPDATE users  set name = $1, age = $2, country = $3, email = $4, password = $5 WHERE id = $6', [name, age, country, email, hashPass, id]);
+        res.json('new user updated');
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            res.status(403).json({ message: error.message });
+        }
+    }
+});
+exports.updateUser = updateUser;
