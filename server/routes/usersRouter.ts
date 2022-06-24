0a6d2ae8fb081c4252b4719e1fda324736ca29fc
matiasfeliu92 <matumazparrote@gmail.com>
2022-06-24 10:36:00 -0300

    se agrego el endpoint que devuelve los datos del nuevo controlador de la vista SQL

--- server/routes/usersRouter.ts
+++ server/routes/usersRouter.ts
@@ -1,8 +1,9 @@
 import { Router } from "express"
-import { getAllUsers, getUsersById, updateUser } from "../controllers/userController"
+import { getAllUsers, getAllUsersAndEmpress, getUsersById, updateUser } from "../controllers/userController"
 const router = Router()
 
 router.get('/', getAllUsers)
+router.get('/users-with-empress', getAllUsersAndEmpress)
 router.get('/:id', getUsersById)
 router.put('/update/:id', updateUser)
 
