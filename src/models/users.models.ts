e4c60d808b8ddd52a59fef9a5fb737d44163bfe0
matiasfeliu92 <matumazparrote@gmail.com>
2023-04-18 18:39:41 -0300

    refactor: update db models and add routes for sign up and sign in

--- src/models/users.models.ts
+++ src/models/users.models.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+// import bcrypt from 'bcrypt';
 
 @Entity()
 export class User {
@@ -18,10 +19,21 @@ export class User {
     @Column({ type: 'varchar', length: 30, unique: true })
     email!: string;
 
-    @Column({ type: 'varchar', length: 20 })
+    @Column({ type: 'varchar', length: 100 })
     password!: string;
+
+    // @Column({ type: 'varchar', length: 100 })
+    // encripted_password!: string;
 
     @Column({ type: 'varchar', length: 50 })
     job_occupation!: string;
 
+    // hashPassword () {
+    //     const salt = bcrypt.genSaltSync(10)
+    //     this.password = bcrypt.hashSync(this.password, salt)
+    // }
+
+    // checkIfPasswordMatch(unencryptedPassword: string) {
+    //     return bcrypt.compareSync(unencryptedPassword, this.password);
+    // }
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -6,5 +6,7 @@ const userController = new UserController()
 
 userRouter.get('/', userController.getAllUsers)
 userRouter.get('/:id', userController.getUserById)
+userRouter.post('/signup', userController.signUp)
+userRouter.post('/signin', userController.signIn)
 
 export default userRouter
